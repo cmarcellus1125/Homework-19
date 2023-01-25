79eb36a2bc20c150530aec0f9460893dfbeb4d3f
cmarcellus1125 <chrismarcelluspom@gmail.com>
2023-01-24 18:32:44 -0700

    Finished

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 var modal = document.getElementById("resumeModal");
 
-var btn = document.getElementById("resume");
+var btn = document.getElementsByClassName("resume");
 
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
+var dbtn = document.getElementsByClassName("dbutton");
+
+btn[0].onclick = function() {
+  $(modal).fadeIn('300');
   modal.style.display = "block";
-}
+};
+
+btn[1].onclick = function() {
+  $(modal).fadeIn('300');
+  modal.style.display = "block";
+};
 
 span.onclick = function() {
-  modal.style.display = "none";
-}
+  $(modal).fadeOut('300');
+};
 
 window.onclick = function(event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    $(modal).fadeOut('300');
   }
-}
+};
+
+$(document).on("click",".dbutton",function(e){
+  console.log("click");
+  $(modal).fadeOut('300');
+});
+
+// dbtn.onclick = function () {
+//   $(modal).fadeOut('300');
+//   console.log("click")
+// };
